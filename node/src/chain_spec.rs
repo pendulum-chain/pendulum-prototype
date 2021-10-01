2e1c393d36d816a8dcfc49adeaf6c266dd9f094a
Gonza <gonzalo@satoshipay.io>
2021-10-01 12:50:03 -0300

    Increase faucet amount of PEN tokens by 100

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -157,7 +157,7 @@ fn testnet_genesis(
             balances: endowed_accounts
                 .iter()
                 .cloned()
-                .map(|k| (k, 1000_000_000_000_000_000u128))
+                .map(|k| (k, 100_000_000_000_000_000_000u128))
                 .collect(),
         }),
         pallet_aura: Some(AuraConfig {
